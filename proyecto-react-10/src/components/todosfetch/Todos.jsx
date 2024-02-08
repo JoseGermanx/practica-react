e9fbe1ca8c018182b377ae6e69f6a970086e3c78
Jose Germán Martínez <jgermanmm@gmail.com>
2024-02-08 12:53:21 -0300

    Actualizar diseño de tarjetas de tareas en Todos.jsx

--- proyecto-react-10/src/components/todosfetch/Todos.jsx
+++ proyecto-react-10/src/components/todosfetch/Todos.jsx
@@ -18,11 +18,13 @@ const Todos = () => {
   return (
     <div className='container'>
         {todos.length > 0 ? todos.map(tarea => (
-            <div className='border border-1' key={tarea.id}>
-                <h2>{tarea.title}</h2>
-                <h2>{tarea.id}</h2>
-                <p>{tarea.completed ? 'Completada' : 'Incompleta'}</p>
+            <div className="card mb-4" style={{width: "18rem"}} key={tarea.id}>
+            <div className="card-body">
+              <h5 className="card-title text-info">{tarea.title}</h5>
+              <h6 className="card-subtitle mb-2 text-body-secondary">Tarea n° {tarea.id}</h6>
+              <p className="card-text">{tarea.completed ? "Completada": "Incompleta" }</p>
             </div>
+          </div>
         )): <h1>Cargando......</h1>}
     </div>
   )
